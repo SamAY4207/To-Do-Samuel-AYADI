8763be521d3cc84304866afe42be4eb1e74fe148
SamAY4207 <samuelayadi07@gmail.com>
2026-05-28 13:06:00 +0200

    0.2

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const port = 3000
 // On utilise le moteur de template ejs
 app.set("view engine", "ejs")
 
+
+
 //Mes assets et mon style sont dans le dossier public
 app.use(express.static('public'))
+
+//Permet de lire correctement les données envoyés par le fomulaire
+app.use(express.urlencoded({extended: true}))
 
 //
 app.get('/', (req, res) => {
@@ -16,6 +21,27 @@ app.get('/', (req, res) => {
     }
   res.render("home", {"user" : user})
 })
+
+//Route qui affiche le formulaire pour créer une tâche
+app.get("/ajouter", (req,rep)=>{
+    rep.render("ajouter.ejs")
+})
+
+//Route qui reçoit les données du formulaire
+app.post("/ajouter",(req,rep)=>{
+    console.log(req.body)
+})
+
+
+app.get("/supprimer", (req,rep)=>{
+    rep.render("supprimer   .ejs")
+})
+
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
